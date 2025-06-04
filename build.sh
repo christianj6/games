c236013c031ab5d5ddb3b45405999c682d6a35f9
Johnson, Christian <christian.johnson@bauer.de>
2025-06-04 10:33:29 +0200

    add build.sh and update gitignore

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e 
+
+# change the project dir to build different games
+cd src/00_dasher/build
+./premake5.osx gmake2
+
+# generate compile_commands.json using bear
+cd ..
+# comment out once compile_commands has been generated
+# bear -- make
+make
+
+# run the game 
+./bin/Debug/00_dasher
