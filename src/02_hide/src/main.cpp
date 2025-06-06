4ee0be1bb8027b6ac54cab2735ca8c07e743539b
Johnson, Christian <christian.johnson@bauer.de>
2025-06-06 13:53:46 +0200

    update it

--- src/02_hide/src/main.cpp
+++ src/02_hide/src/main.cpp
@@ -1,19 +1,26 @@
 #include "player.h"
 #include "raylib.h"
+#include "world.h"
 
 int main() {
   SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
   InitWindow(1280, 800, "Hide");
 
-  // TODO: procedurally generated game world
-  auto player = Player();
+  World world;
+  world.add_object(std::make_unique<Player>(Vector2{640, 400}));
 
-  while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(BLACK);
-    player.draw();
-    EndDrawing();
+  while (!WindowShouldClose()) { 
+    float dt = GetFrameTime();   
+
+    BeginDrawing();         
+    ClearBackground(BLACK); 
+
+    world.update(dt); 
+    world.draw();    
+
+    EndDrawing(); 
   }
-  CloseWindow();
-  return 0;
+
+  CloseWindow(); 
+  return 0;     
 }
