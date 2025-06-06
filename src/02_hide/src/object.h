6a6e167baae640eac2be7001aa6cc91e28dcad2e
Johnson, Christian <christian.johnson@bauer.de>
2025-06-06 13:53:39 +0200

    remove comment

--- src/02_hide/src/object.h
+++ src/02_hide/src/object.h
@@ -3,10 +3,11 @@
 
 class GameObject {
   public:
-    // special way to intialize
     GameObject(Vector2 starting_position = {0,0}) : position(starting_position) {}
+
     virtual void draw() = 0;
     virtual void update(float dt) = 0;
+
   protected:
     Vector2 position;
 };
