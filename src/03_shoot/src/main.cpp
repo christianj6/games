5e0c38bccfb0cf6c127da61eb0308348da33c42a
Johnson, Christian <christian.johnson@bauer.de>
2025-06-15 09:14:25 +0200

    update readme

--- src/03_shoot/src/main.cpp
+++ src/03_shoot/src/main.cpp
@@ -1,7 +1,6 @@
 #include "game.h"
 #include "raylib.h"
 
-// TODO: player cannot pass through walls or obstacles
 // TODO: floating ball enemy which hunts the player as in 02_hide game
 // TODO: game over if enemy gets too close to the player
 // TODO: shooting mechanic which allows the player to shoot the enemy and "win"
