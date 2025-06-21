d3fe8d6c8c00aa890f0c9dd36c10ae356a2a9d70
christianj6 <chr.a.johnson@gmail.com>
2025-06-21 10:24:36 +0200

    remove comment

--- src/03_shoot/src/main.cpp
+++ src/03_shoot/src/main.cpp
@@ -1,7 +1,6 @@
 #include "game.h"
 #include "raylib.h"
 
-// TODO: floating ball enemy which hunts the player as in 02_hide game
 // TODO: game over if enemy gets too close to the player
 // TODO: shooting mechanic which allows the player to shoot the enemy and "win"
 // TODO: weapon which allows the player to access the shooting mechanic
