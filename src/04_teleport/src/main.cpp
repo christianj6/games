8452764a74e092580c4e4c5ce546e080f496d6ed
christianj6 <chr.a.johnson@gmail.com>
2025-06-23 07:52:34 +0200

    line up some todos for the teleport project

--- src/04_teleport/src/main.cpp
+++ src/04_teleport/src/main.cpp
@@ -1,5 +1,24 @@
 #include "raylib.h"
 
+// TODO: more modular abstractions for colliders, controllers, etc.
+// TODO: abstraction layer for collision detection and vector operations
+// TODO: files are organized into directories for different layers / systems
+// TODO: slightly larger game world, made possible through voxel-based rendering
+// TODO: pseudo voxel world: implement a world state with Matrix, abstractions update this
+// TODO: voxels are either visible or not; simple "exposure" detection based on surrounding blocks and open space
+// TODO: rely on raylib's mesh instancing to draw them; a reasonable start
+// TODO: cache visibility to avoid repeated calculations
+// TODO: hud allows for easy toggle of debug information for development
+// TODO: xbox controller support
+// TODO: teleportation mechanic allows player to blink forwards and on top of obstacles
+// TODO: jump mechanic which pairs with teleportation
+// TODO: world layout regularly changes like a shifting labyrinth
+// TODO: enemies have real grid-based pathfinding for more intense hunting of the player
+// TODO: enemies are more interesting or there exist additional "creates" with cooler visuals
+// TODO: consider throwing in some visual clutter and additional lighting effects to make the game world pop
+// TODO: web assembly build?
+
+
 int main() {
   SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
   InitWindow(1280, 800, "Hello Raylib");
