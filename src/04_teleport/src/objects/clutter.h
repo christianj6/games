933f28951538aab5c4cf3f917b11ec60543ea357
christianj6 <chr.a.johnson@gmail.com>
2025-06-25 06:47:55 +0200

    add comment

--- src/04_teleport/src/objects/clutter.h
+++ src/04_teleport/src/objects/clutter.h
@@ -1,2 +1,4 @@
 // TODO: additional objects in the world to make it more visually interesting
+// TODO: try to use mesh instancing and some kind of noise / simulation to 
+// spawn these in the world and along the columns in an interesting way
 // TODO: keep the scope under control
