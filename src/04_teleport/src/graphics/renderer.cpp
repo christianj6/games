1024c7523552dba808e835694249dcee39a4d33e
christianj6 <chr.a.johnson@gmail.com>
2025-06-25 21:09:07 +0200

    update light colors so they are a bit more interesting and remove the moving light

--- src/04_teleport/src/graphics/renderer.cpp
+++ src/04_teleport/src/graphics/renderer.cpp
@@ -33,33 +33,21 @@ void Renderer::update(Camera camera) {
   float position[3] = {camera.position.x, camera.position.y, camera.position.z};
   SetShaderValue(shader, shader.locs[SHADER_LOC_VECTOR_VIEW], position,
                  SHADER_UNIFORM_VEC3);
-
-  // Update moving light position in a circular pattern
-  moving_light_angle += 0.5f * GetFrameTime();
-  float radius = 40.0f;
-  moving_light.position =
-      (Vector3){cosf(moving_light_angle) * radius,
-                8.0f + sinf(moving_light_angle * 0.5f) *
-                           2.0f, // Add some vertical movement
-                sinf(moving_light_angle) * radius};
-
-  UpdateLightValues(shader, moving_light);
 }
 
 void Renderer::configure_lighting() {
   const float AREA_SIZE = 80.0f;
   const float CORNER_HEIGHT = 15.0f;
 
-  Light light2 =
-      CreateLight(LIGHT_POINT, (Vector3){AREA_SIZE, CORNER_HEIGHT, AREA_SIZE},
-                  Vector3Zero(), (Color){200, 100, 100, 255}, shader);
-  Light light3 =
-      CreateLight(LIGHT_POINT, (Vector3){-AREA_SIZE, CORNER_HEIGHT, -AREA_SIZE},
-                  Vector3Zero(), (Color){100, 200, 100, 255}, shader);
-  Light light4 =
-      CreateLight(LIGHT_POINT, (Vector3){AREA_SIZE, CORNER_HEIGHT, -AREA_SIZE},
-                  Vector3Zero(), (Color){100, 100, 200, 255}, shader);
+  CreateLight(LIGHT_POINT, (Vector3){AREA_SIZE, CORNER_HEIGHT, -AREA_SIZE},
+              Vector3Zero(), DARKPURPLE, shader);
 
-  moving_light = CreateLight(LIGHT_POINT, (Vector3){0.0f, 8.0f, 0.0f},
-                             Vector3Zero(), (Color){255, 50, 50, 255}, shader);
+  CreateLight(LIGHT_POINT, (Vector3){-AREA_SIZE, CORNER_HEIGHT, AREA_SIZE},
+              Vector3Zero(), DARKGREEN, shader);
+
+  CreateLight(LIGHT_POINT, (Vector3){-AREA_SIZE, CORNER_HEIGHT, -AREA_SIZE},
+              Vector3Zero(), DARKBLUE, shader);
+
+  CreateLight(LIGHT_POINT, (Vector3){AREA_SIZE, CORNER_HEIGHT, AREA_SIZE},
+              Vector3Zero(), RED, shader);
 }
