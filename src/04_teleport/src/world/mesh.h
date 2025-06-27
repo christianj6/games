8658d538f8d1a6285e53918417b52c75d31adf08
christianj6 <chr.a.johnson@gmail.com>
2025-06-27 12:03:53 +0200

    remove comment

--- src/04_teleport/src/world/mesh.h
+++ src/04_teleport/src/world/mesh.h
@@ -2,8 +2,6 @@
 #include "raylib.h"
 #include <vector>
 
-// TODO: simple grid-based nav mesh
-
 std::vector<Eigen::Matrix<bool, Eigen::Dynamic, Eigen::Dynamic>>
 slice_voxel_space(
     const std::vector<Eigen::Matrix<bool, Eigen::Dynamic, Eigen::Dynamic>> &,
