79a7885476e629c22b9a9f532c4eab686a354ffe
christianj6 <chr.a.johnson@gmail.com>
2025-06-30 12:28:38 +0200

    update path

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # alternative command to hook into windows from wsl
-cmd.exe /c "C:\Users\Christian\Desktop\repos\games\src\04_teleport\build.bat"
+cmd.exe /c "C:\Users\Christian\Desktop\repos\games\build.bat"
