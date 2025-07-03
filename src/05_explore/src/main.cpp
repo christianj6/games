448595f8cb10056e93ef23d6a465e933f1a79410
Christian Johnson <chr.a.johnson@gmail.com>
2025-07-03 21:10:00 +0200

    add window boilerplate to test build

--- src/05_explore/src/main.cpp
+++ src/05_explore/src/main.cpp
@@ -2,7 +2,7 @@
 /*
  * clean architecture
  * better performance than teleport
- * reduce invest in additional tooling, in order to 
+ * reduce invest in additional tooling, in order to
  * focus more on core game implementation
  * make it silly; have fun with it
  */
@@ -21,13 +21,12 @@
  * NPCs roam around a specified area
  * player can "chat" with NPCs in a limited, scripted way
  * enemies attack in waves according to a timer
- * between waves, NPCs periodically request assets which are used to attack enemies
- * player must explore the open world to find the assets, while avoiding enemies
- * assets can be used to create traps, or NPCs throw/shoot them
- * player can attack enemies by getting close but it is very risky
- * NPCs can heal the player between waves
- * if all NPCs die you lose
- * if make it through all the waves you win
+ * between waves, NPCs periodically request assets which are used to attack
+ * enemies player must explore the open world to find the assets, while avoiding
+ * enemies assets can be used to create traps, or NPCs throw/shoot them player
+ * can attack enemies by getting close but it is very risky NPCs can heal the
+ * player between waves if all NPCs die you lose if make it through all the
+ * waves you win
  */
 
 // TODO: expanded teleportation
@@ -49,3 +48,20 @@
 /*
  * consider renaming to "defend"
  */
+#include "raylib.h"
+
+int main() {
+  SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+  SetTargetFPS(60);
+  InitWindow(400, 200, "Explore");
+  DisableCursor();
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    EndDrawing();
+  }
+  CloseWindow();
+
+  return 0;
+}
