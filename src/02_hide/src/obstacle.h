1a86ebcdcf2b8eaaa3993483cda27f805367a485
christianj6 <chr.a.johnson@gmail.com>
2025-07-03 13:37:18 +0200

    fix up hide

--- src/02_hide/src/obstacle.h
+++ src/02_hide/src/obstacle.h
@@ -5,9 +5,9 @@
 // public inheritance means we take everything from parent 'as-is'
 class Obstacle : public GameObject {
 public:
-    Obstacle(Vector2 starting_position);
-    void draw() override;
-    void update(float dt, bool allow_movement = false) override;
+  Obstacle(Vector2 starting_position);
+  void draw() override;
+  void update(float dt, bool allow_movement = false) override;
 };
 
 std::vector<Obstacle> get_world_obstacles(int n);
--- src/02_hide/src/world.h
+++ src/02_hide/src/world.h
@@ -1,35 +1,35 @@
 #pragma once
-#include <vector>
-#include <memory>
 #include "object.h"
 #include "player.h"
+#include <memory>
+#include <vector>
 
 class World {
 private:
-    // >> closes the type declaration when we have nested, templated objects
-    std::vector<std::unique_ptr<GameObject>> objects;
-    std::unique_ptr<Player> player_ptr = nullptr;
-    Camera2D camera;
+  // >> closes the type declaration when we have nested, templated objects
+  std::vector<std::unique_ptr<GameObject>> objects;
+  std::unique_ptr<Player> player_ptr = nullptr;
+  Camera2D camera;
 
-    bool is_on_screen(const Vector2& position) const;
+  bool is_on_screen(const Vector2 &position) const;
 
 public:
-    World() {
-        // TODO: shift to implementation file
-        camera.target = {0, 0};
-        camera.offset = {GetScreenWidth()/2.0f, GetScreenHeight()/2.0f};
-        camera.rotation = 0.0f;
-        camera.zoom = 1.0f;
-    }
-    ~World() = default;
+  World() {
+    // TODO: shift to implementation file
+    camera.target = {0, 0};
+    camera.offset = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+  }
+  ~World() = default;
 
-    void add_object(std::unique_ptr<GameObject> object);
-    bool update(float dt);
-    void draw();         
-    
-    // returns raw pointer to the player without transferring ownership
-    Player* get_player_ptr() const { return player_ptr.get(); }
-    // we move the incoming pointer to the class attr, transferring ownership
-    void set_player(std::unique_ptr<Player> p) { player_ptr = std::move(p); }
-    std::vector<GameObject*> get_obstacles() const;
+  void add_object(std::unique_ptr<GameObject> object);
+  bool update(float dt);
+  void draw();
+
+  // returns raw pointer to the player without transferring ownership
+  Player *get_player_ptr() const { return player_ptr.get(); }
+  // we move the incoming pointer to the class attr, transferring ownership
+  void set_player(std::unique_ptr<Player> p) { player_ptr = std::move(p); }
+  std::vector<GameObject *> get_obstacles() const;
 };
