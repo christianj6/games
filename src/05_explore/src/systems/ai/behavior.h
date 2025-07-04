20a2080043eaf64b4f655c82ac94c3b9279b5862
christianj6 <chr.a.johnson@gmail.com>
2025-07-04 16:10:57 +0200

    add comments to ai module

---
+++ src/05_explore/src/systems/ai/behavior.h
@@ -0,0 +1,3 @@
+// TODO: encapsulate behavior but behavior trees are unique
+// TODO: enemies see you but have a period before transitioning to alerted state
+// TODO: put a bunch of utility functions concerning if an NPC sees the player etc here.
