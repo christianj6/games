bed7b65d348e1ed968dd84015410a98b567d9ef3
Christian Johnson <chr.a.johnson@gmail.com>
2025-07-07 14:07:55 +0200

    pass by ref

--- src/05_explore/src/actors/player.h
+++ src/05_explore/src/actors/player.h
@@ -5,7 +5,7 @@
 class Player : public Actor {
 public:
   Player();
-  Vector3 update(float, Blackboard) override;
+  Vector3 update(float, Blackboard&) override;
   void draw() override;
   Camera3D get_camera() {return camera;}
 private:
