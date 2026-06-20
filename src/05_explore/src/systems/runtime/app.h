0b26082fb5de24fd3e973c38767016cf6657dcf1
christianj6 <chr.a.johnson@gmail.com>
2026-06-20 15:36:27 +0200

    start app in game and add todo about it

--- src/05_explore/src/systems/runtime/app.h
+++ src/05_explore/src/systems/runtime/app.h
@@ -8,7 +8,7 @@ public:
   bool run(bool);
 
 private:
-  ApplicationState current_state = ApplicationState::MAIN_MENU;
+  ApplicationState current_state = ApplicationState::GAME; // TODO: change back to MAIN_MENU
   Game game;
   void main_menu();
 };
