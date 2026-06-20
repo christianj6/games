d028f708d6a22aaae6271b2442e3a40a45f8a3a2
christianj6 <chr.a.johnson@gmail.com>
2026-06-20 19:40:47 +0200

    try another approach

--- src/05_explore/resources/shaders/glsl330/lighting.fs
+++ src/05_explore/resources/shaders/glsl330/lighting.fs
@@ -30,12 +30,9 @@ void main()
 {
     vec4 texelColor = texture(texture0, fragTexCoord);
     vec3 normal     = normalize(fragNormal);
-    vec3 viewD      = normalize(viewPos - fragPosition);
     vec3 tint       = colDiffuse.rgb * fragColor.rgb;
 
-    vec3 diffuse  = vec3(0.0);
-    vec3 specular = vec3(0.0);
-
+    vec3 diffuse = vec3(0.0);
     for (int i = 0; i < MAX_LIGHTS; i++)
     {
         if (lights[i].enabled == 1)
@@ -46,20 +43,12 @@ void main()
             else
                 light = normalize(lights[i].position - fragPosition);
 
-            float NdotL = max(dot(normal, light), 0.0);
-            diffuse += lights[i].color.rgb * NdotL;
-
-            if (NdotL > 0.0)
-            {
-                vec3 halfV   = normalize(light + viewD);
-                float specCo = pow(max(dot(normal, halfV), 0.0), 64.0);
-                specular    += lights[i].color.rgb * specCo;
-            }
+            diffuse += lights[i].color.rgb * max(dot(normal, light), 0.0);
         }
     }
 
     vec3 ambientColor = (ambient.rgb / 10.0) * tint;
-    vec3 color        = texelColor.rgb * (tint * diffuse + specular + ambientColor);
+    vec3 color        = texelColor.rgb * (tint * diffuse + ambientColor);
 
     finalColor = clamp(vec4(color, texelColor.a * colDiffuse.a), 0.0, 1.0);
     finalColor = pow(finalColor, vec4(1.0 / 2.2));
